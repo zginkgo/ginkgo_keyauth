a0bf3a45c5faff4e1f56149bd6223c403156dc27
You-Men <y@ydeMacBook-Pro.local>
2022-10-12 13:14:22 +0800

    add auth label v0.0.2

--- common/utils/header.go
+++ common/utils/header.go
@@ -9,6 +9,7 @@ import (
 // 1. URL Query String ?
 // 2. Custom Header ?
 // 3. Authorization Header
+// test go mod cache
 func GetToken(r *http.Request) (accessToken string) {
 	auth := r.Header.Get("Authorization")
 	al := strings.Split(auth, " ")
